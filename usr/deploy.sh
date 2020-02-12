bdefa04ba550ff6f49774876aedb2ac127e8fad1
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-02-12 08:17:23 +0100

    Updated usr scripts

--- usr/deploy.sh
+++ usr/deploy.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 # Deploy usr apps into the first partition
-echo Deploying usr apps into the agency partition...
+echo Deploying usr apps into the first partition...
 cd ../filesystem
 ./mount.sh 1
-sudo cp ../usr/*.elf fs/
+sudo cp ../usr/out/*.elf fs/
 sudo ./umount.sh
