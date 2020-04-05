be36cc6d14ba956553f46605816712d35d527439
Nikolaos Garanis <945263-nyg@users.noreply.gitlab.com>
2020-04-05 14:00:54 +0200

    Make PL111 driver values easier to change (see #2).

--- so3/devices/fb/pl111.c
+++ so3/devices/fb/pl111.c
@@ -30,10 +30,55 @@
 #define CLCD_TIM3	0x0000000c
 #define CLCD_UBAS	0x00000010
 #define CLCD_LBAS	0x00000014
-#define CLCD_PL111_CNTL	0x00000018
-#define CLCD_PL111_IENB	0x0000001c
+#define CLCD_CNTL	0x00000018
+#define CLCD_IENB	0x0000001c
 
-#define VRAM_BASE	0x18000000
+/* Timing0 register values */
+#define HBP (39 << 24)
+#define HFP (23 << 16)
+#define HSW (95 <<  8)
+#define PPL (39 <<  2) /* 16 * (PPL + 1) = horizontal res */
+
+/* Timing1 register values */
+#define VBP ( 32 << 24)
+#define VFP ( 11 << 16)
+#define VSW (  1 << 10)
+#define LPP (479 <<  0) /* LPP + 1 = vertical res */
+
+/* Timing2 register values */
+#define PCD_HI (  0 << 27)
+#define BCD    (  1 << 26)
+#define CPL    (639 << 16) /* for TFT: PPL - 1 */
+#define IOE    (  0 << 14)
+#define IPC    (  1 << 13)
+#define IHS    (  1 << 12)
+#define IVS    (  1 << 11)
+#define ACB    (  0 <<  6)
+#define CLKSEL (  0 <<  5)
+#define PCD_LO (  0 <<  0)
+
+/* Timing3 register values */
+#define LEE (0 << 16)
+#define LED (0 <<  0)
+
+/* Framebuffer base addresses (first 2 bits must be 0) */
+#define LCDUPBASE 0x18000000 /* upper panel, corresponds to VRAM */
+#define LCDLPBASE        0x0 /* lower panel */
+
+/* Control register values */
+#define WATERMARK (0 << 16)
+#define LCDVCOMP  (1 << 12) /* generate interrupt at start of back porch */
+#define LCDPWR    (1 << 11) /* power display */
+#define BEPO      (0 << 10) /* little-endian pixel ordering */
+#define BEBO      (0 <<  9) /* little-endian byte order */
+#define BGR       (0 <<  8) /* RGB */
+#define LCDDUAL   (0 <<  7) /* single panel */
+#define LCDMONO8  (0 <<  6) /* unused for TFT */
+#define LCDTFT    (1 <<  5) /* LCD is TFT */
+#define LCDBW     (0 <<  4) /* unused for TFT */
+#define LCDBPP    (6 <<  1) /* 16bpp 5:6:5 mode */
+#define LCDEN     (1 <<  0) /* enable display */
+
 
 void *mmap(int fd, uint32_t virt_addr, uint32_t page_count);
 
@@ -44,72 +89,28 @@ struct file_operations pl111_ops = {
 
 /*
  * Initialisation of the PL111 CLCD Controller.
- * References to the Linux driver (amba-clcd.c) are left for documentation.
  */
 int pl111_init(dev_t *dev)
 {
-	/* amba-clcd.c:555 clcdfb_register: disable interrupts */
-	iowrite32(dev->base + CLCD_PL111_IENB, 0);
+	/* Disable interrupts. */
+	iowrite32(dev->base + CLCD_IENB, 0);
 
-	/* amba-clcd.c:327 clcdfb_set_par: set timing registers */
+	/* Set timing registers. */
+	iowrite32(dev->base + CLCD_TIM0, HBP | HFP | HSW | PPL);
+	iowrite32(dev->base + CLCD_TIM1, VBP | VFP | VSW | LPP);
+	iowrite32(dev->base + CLCD_TIM2, PCD_HI | BCD | CPL | IOE | IPC | IHS | IVS | ACB | CLKSEL | PCD_LO);
+	iowrite32(dev->base + CLCD_TIM3, LEE | LED);
 
-	// 00100111 00010111 01011111 10011100
-	// HBP: 00100111 39 0x27
-	// HFP: 00010111 23 0x17
-	// HSW: 01011111 95 0x5f
-	// PPL:   100111 39 0x27 => 16 * (39 + 1) = 640px horizontal
-	iowrite32(dev->base + CLCD_TIM0, 0x27175f9c);
-
-	// 00100000 00001011 00000101 11011111
-	// VBP:   00100000  32  0x20
-	// VFP:   00001011  11   0xb
-	// VSW:     000001   1   0x1
-	// LPP: 0111011111 479 0x1df => 479 + 1 = 480px vertical
-	iowrite32(dev->base + CLCD_TIM1, 0x200b05df);
-
-	// 00000110 01111111 00111000 00000000
-	// PCD_HI:      00000
-	// BCD:             1
-	// CPL:    1001111111 639 0x27f
-	// Unreg (1 bit)
-	// IOE:             0
-	// IPC:             1
-	// IHS:             1
-	// IVS:             1
-	// ACB:        000000
-	// CLKSEL:          0
-	// PDC_LO:      00000
-	iowrite32(dev->base + CLCD_TIM2, 0x067f3800);
-
-	// 0
-	iowrite32(dev->base + CLCD_TIM3, 0);
-
-	/* amba-clcd.c:62 clcdfb_set_start: write_regs fb address */
-	iowrite32(dev->base + CLCD_UBAS, 0x18000000);
-	iowrite32(dev->base + CLCD_LBAS, 0x1804b000);
+	/* Set frame address */
+	iowrite32(dev->base + CLCD_UBAS, LCDUPBASE);
+	if (LCDDUAL) {
+		iowrite32(dev->base + CLCD_LBAS, LCDLPBASE);
+	}
 
 	/* amba-clcd.c:113 clcdfb_enable: enable and power on */
+	iowrite32(dev->base + CLCD_CNTL, WATERMARK | LCDVCOMP | LCDPWR | BEPO | BEBO | BGR | LCDDUAL | LCDMONO8 | LCDTFT | LCDBW | LCDBPP | LCDEN);
 
-	// enable
-	iowrite32(dev->base + CLCD_PL111_CNTL, 0x112d);
-
-	// power on
-	// 000000000000000 0 00 01 1 0 0 1 0 0 1 0 110 1
-	// WATERMARK: 0
-	// Unreg (2 bits)
-	// LCDVCOMP:  01 => interrupt at start of back porch
-	// LCDPWR:    1 => powered on
-	// BEPO:      0 => unused for 16bpp
-	// BEBO:      0 => little-endian byte order
-	// BGR:       1 => blue green red
-	// LCDDUAL:   0 => single panel LCD
-	// LCDMONO8:  0 => unused for 16bpp mode
-	// LCDTFT:    1 => LCD is TFT
-	// LCDBW:     0 => color (unused for TFT)
-	// LCDBPP:  110 => 16 bits per pixel, 5:6:5 mode
-	// LCDEN:     1 => enabled
-	iowrite32(dev->base + CLCD_PL111_CNTL, 0x182d);
-
+	/* Register framebuffer fops. */
 	if (register_fb_ops(&pl111_ops)) {
 		printk("%s: pl111 initialised but could not register fops.", __func__);
 		return -1;
@@ -125,7 +126,7 @@ void *mmap(int fd, uint32_t virt_addr, uint32_t page_count)
 
 	for (i = 0; i < page_count; i++) {
 		/* Map a process' virtual page to the physical one (here the VRAM). */
-		page = VRAM_BASE + i * PAGE_SIZE;
+		page = LCDUPBASE + i * PAGE_SIZE;
 		create_mapping(pcb->pgtable, virt_addr + (i * PAGE_SIZE), page, PAGE_SIZE, false);
 		add_page_to_proc(pcb, phys_to_page(page));
 	}
