7745d1de8d2594c1f731c7245bc5efc585274490
Julien Quartier <julien.quartier@bluewin.ch>
2020-05-06 21:11:24 +0200

    Userspace ping app (prototype)

--- usr/src/ping.c
+++ usr/src/ping.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -56,10 +58,14 @@ unsigned short checksum(void *b, int len)
 
 
 int main(int argc, char **argv) {
-    int s, i, msg_count = 0;
-
+    int s, i = 0, msg_count = 0;
+    float elapsed_time = 0;
+    //socklen_t addr_len;
     struct ping_pkt packet;
-    struct sockaddr_in ping_addr;
+    struct sockaddr_in ping_addr/*, recv_addr*/;
+    struct timeval tv_out, start, end;
+    tv_out.tv_sec = 1;
+    tv_out.tv_usec = 0;
 
     s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 
@@ -72,37 +78,63 @@ int main(int argc, char **argv) {
 
     // TODO set timeout
 
+    setsockopt(s, 0xfff, 0x1005, (const char*)&tv_out, sizeof(struct timeval));
+    setsockopt(s, 0xfff, 0x1006, (const char*)&tv_out, sizeof(struct timeval));
 
-    // TODO loop from here
-    inet_pton(AF_INET, "192.168.1.1", &ping_addr.sin_addr);
 
-    ping_addr.sin_family = AF_INET;
-    ping_addr.sin_port = 0; // ICMP -> no port
+    while(i++ < 10){
+        // TODO loop from here
+        inet_pton(AF_INET, "10.0.2.2", &ping_addr.sin_addr);
 
-    memset(&packet, 0, sizeof(struct ping_pkt));
+        printf("%d: \n", i);
 
-    packet.hdr.type = ICMP_ECHO;
-    packet.hdr.un.echo.id = getpid();
+        ping_addr.sin_family = AF_INET;
+        ping_addr.sin_port = 0; // ICMP -> no port
 
-    for(i = 0; i < sizeof(packet.msg) - 1; i++) {
-        packet.msg[i] = i + '0';
+        memset(&packet, 0, sizeof(struct ping_pkt));
+
+        packet.hdr.type = ICMP_ECHO;
+        packet.hdr.un.echo.id = getpid();
+
+        for(i = 0; i < sizeof(packet.msg) - 1; i++) {
+            packet.msg[i] = i + '0';
+        }
+        packet.msg[i] = 0;
+        packet.hdr.un.echo.sequence = msg_count++;
+        packet.hdr.checksum = checksum(&packet, sizeof(packet));
+
+        gettimeofday(&start, NULL);
+
+        if(sendto(s, &packet, sizeof(packet), 0, (struct sockaddr*)&ping_addr, sizeof(ping_addr)) <= 0){
+            printf("Packet sending failed!!\n");
+            continue;
+        }
+
+        if(recvfrom(s, &packet, sizeof(packet), 0, NULL, NULL) <= 0 && msg_count > 1){
+            printf("Packet receive failed!!\n");
+            continue;
+        }
+
+        gettimeofday(&end, NULL);
+
+
+        elapsed_time = end.tv_usec / 1000.0 + end.tv_sec * 1000 - (start.tv_usec / 1000.0 + start.tv_sec * 1000);
+
+        if(!(packet.hdr.type == 69 && packet.hdr.code == 0)){
+            printf("Error... Packet received with ICMP type %d code %d\n", packet.hdr.type, packet.hdr.code);
+        } else {
+            printf("Packet Received ms %d\n", elapsed_time);
+        }
+
+        sleep(1);
     }
-    packet.msg[i] = 0;
-    packet.hdr.un.echo.sequence = msg_count++;
-    packet.hdr.checksum = checksum(&packet, sizeof(packet));
 
-    if(sendto(s, &packet, sizeof(packet), 0, (struct sockaddr*)&ping_addr, sizeof(ping_addr)) <= 0){
-        printf("Packet sending failed!!\n");
-        goto end;
-    }
+    return 0;
 
 
 
-
-
-
-    end:
+    /*end:
 
     close(s);
-    return 0;
+    return 0;*/
 }
