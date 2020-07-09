24438d89df8b7a257b3501447d337493fb82e0e4
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-07-09 18:15:12 +0200

    Updates

--- rootfs/umount_initrd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Usage: ./umount_initrd <board>"
-echo "Here: board is $1"
-
-# Currently not using cpio but fat
-#../tools/umount_cpio $PWD/board/$1/initrd.cpio
-sudo umount initrd
-
-
