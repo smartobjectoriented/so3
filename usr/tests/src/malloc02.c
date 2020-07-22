e9979502014b7d1f23f0c3ed1d9cd68ac270dee8
Rick Wertenbroek <rick.wertenbroek@heig-vd.ch>
2020-07-22 16:26:29 +0200

    Added formatting to the test so that it passes

--- usr/tests/src/malloc02.c
+++ usr/tests/src/malloc02.c
@@ -3,23 +3,28 @@
 #include <stdio.h>
 #include <limits.h>
 
+#include "test.h"
+
 int main()
 {
     void *ptr = malloc(INT_MAX);
 
     //ASSERT_TRUE(ptr != nullptr);
     if (ptr == NULL) {
-        printf("[PASS] malloc() returned a nullptr\n");
+        //printf("[PASS] malloc() returned a nullptr\n");
+        SO3_TEST_SUCCESS("malloc() returned a nullptr\n");
         return 0;
-    }
-    //ASSERT_LE(100U, malloc_usable_size(ptr));
-//    if (malloc_usable_size(ptr) < 100) {
-//        printf("[FAIL] malloc() usable size too small\n");
-//        return -1;
-//    }
+    } else {
+        //ASSERT_LE(100U, malloc_usable_size(ptr));
+        //    if (malloc_usable_size(ptr) < 100) {
+        //        printf("[FAIL] malloc() usable size too small\n");
+        //        return -1;
+        //    }
 
-    free(ptr);
+        free(ptr);
     
-    printf("[FAIL] malloc() allocated an osbscene amount of bytes\n");
-    return 0;
+        //printf("[FAIL] malloc() allocated an osbscene amount of bytes\n");
+        SO3_TEST_FAIL("malloc() allocated an osbscene amount of bytes\n");
+        return -1;
+    }
 }
