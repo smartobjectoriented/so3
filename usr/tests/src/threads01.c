1700aa4626b5eb34422c6998808253679350a57e
Rick Wertenbroek <rick.wertenbroek@heig-vd.ch>
2020-07-22 16:20:43 +0200

    Added formatting to the test so that it passes

--- usr/tests/src/threads01.c
+++ usr/tests/src/threads01.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "test.h"
 #define NUM_THREADS     5
 
 #define RETURN_VALUE ((void *)0xdeadc0de)
@@ -24,7 +26,8 @@ int main (int argc, char *argv[])
         printf("In main: creating thread %ld\n", t);
         rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
         if (rc) {
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            //printf("ERROR; return code from pthread_create() is %d\n", rc);
+            SO3_TEST_FAIL("return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
     }
@@ -34,16 +37,19 @@ int main (int argc, char *argv[])
         // https://stackoverflow.com/questions/47222124/why-the-second-argument-to-pthread-join-is-a-a-pointer-to-a-pointer
         rc = pthread_join(threads[t], &ret);
         if (rc) {
-            printf("ERROR; return code from pthread_join() is %d\n", rc);
+            //printf("ERROR; return code from pthread_join() is %d\n", rc);
+            SO3_TEST_FAIL("return code from pthread_join() is %d\n", rc);
             exit(-1);
         }
         if (ret != RETURN_VALUE) {
-            printf("ERROR; thread did not return correct value\nExpected : 0x%08x Received : 0x%08x\n", RETURN_VALUE, ret);
+            //printf("ERROR; thread did not return correct value\nExpected : 0x%08x Received : 0x%08x\n", RETURN_VALUE, ret);
+            SO3_TEST_FAIL("thread did not return correct value\nExpected : 0x%08x Received : 0x%08x\n", RETURN_VALUE, ret);
             exit(-1);
         } else {
             printf("Thread %d correctly returned : 0x%08x\n", t, ret);
         }
     }
 
+    SO3_TEST_SUCCESS("thread test did pass correctly\n");
     return 0;
 }
