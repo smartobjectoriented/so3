79c116610dd660e2df2998ae8eaf95d8340e4710
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-08-11 11:23:58 +0200

    Cleanings

--- rootfs/create_ramfs.sh
+++ rootfs/create_ramfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#create image first
+# Create image first
+
+# Make sure the dir exists
+mkdir -p board/$1
+
 dd if=/dev/zero of=board/$1/rootfs.fat bs=1024 count=1024
 DEVLOOP=$(sudo losetup --partscan --find --show board/$1/rootfs.fat)
 
