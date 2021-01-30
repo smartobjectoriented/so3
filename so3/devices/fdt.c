e064b5eebd942a1adee204b53dc0c272c5ef2263
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-01-30 14:51:55 +0100

    Fixed issues in dt management and added find_or_add_subnode()

--- so3/devices/fdt.c
+++ so3/devices/fdt.c
@@ -107,14 +107,11 @@ int fdt_get_int(void *fdt_addr, void *dev, const char *name) {
 const struct fdt_property *fdt_find_property(void *fdt_addr, int offset, const char *propname) {
 	const struct fdt_property *prop;
 
-	while (true) {
-
-		prop = fdt_get_property((const void *) fdt_addr, offset, propname, NULL);
-		if (prop)
-			return prop;
-	}
-
-	return NULL;
+	prop = fdt_get_property(fdt_addr, offset, propname, NULL);
+	if (prop)
+		return prop;
+	else
+		return NULL;
 }
 
 int fdt_property_read_string(void *fdt_addr, int offset, const char *propname, const char **out_string) {
@@ -141,7 +138,6 @@ int fdt_property_read_u32(void *fdt_addr, int offset, const char *propname, u32 
 
 	return -1;
 }
-
 
 int fdt_find_node_by_name(void *fdt_addr, int parent, const char *nodename) {
 	int node;
@@ -170,6 +166,56 @@ int fdt_find_compatible_node(void *fdt_addr, char *compat) {
 	int offset;
 
 	offset = fdt_node_offset_by_compatible(fdt_addr, 0, compat);
+
+	return offset;
+}
+/*
+ * fdt_pack_reg - pack address and size array into the "reg"-suitable stream
+ */
+int fdt_pack_reg(const void *fdt, void *buf, u64 *address, u64 *size)
+{
+	int address_cells = fdt_address_cells(fdt, 0);
+	int size_cells = fdt_size_cells(fdt, 0);
+	char *p = buf;
+
+	if (address_cells == 2)
+		*(fdt64_t *)p = cpu_to_fdt64(*address);
+	else
+		*(fdt32_t *)p = cpu_to_fdt32(*address);
+
+	p += 4 * address_cells;
+
+	if (size_cells == 2)
+		*(fdt64_t *)p = cpu_to_fdt64(*size);
+	else
+		*(fdt32_t *)p = cpu_to_fdt32(*size);
+
+	p += 4 * size_cells;
+
+	return p - (char *)buf;
+}
+
+/**
+ * fdt_find_or_add_subnode() - find or possibly add a subnode of a given node
+ *
+ * @fdt: pointer to the device tree blob
+ * @parentoffset: structure block offset of a node
+ * @name: name of the subnode to locate
+ *
+ * fdt_subnode_offset() finds a subnode of the node with a given name.
+ * If the subnode does not exist, it will be created.
+ */
+int fdt_find_or_add_subnode(void *fdt, int parentoffset, const char *name)
+{
+	int offset;
+
+	offset = fdt_subnode_offset(fdt, parentoffset, name);
+
+	if (offset == -FDT_ERR_NOTFOUND)
+		offset = fdt_add_subnode(fdt, parentoffset, name);
+
+	if (offset < 0)
+		printk("%s: %s: %s\n", __func__, name, fdt_strerror(offset));
 
 	return offset;
 }
