34346a8433f17922d991ecb29a5be135bf0005c5
Nicolas Müller <nicolas.muller1@heig-vd.ch>
2021-06-10 15:43:59 +0200

    can make so3 kernel

---
+++ so3/arch/riscv64/asm-offsets.c
@@ -0,0 +1,10 @@
+//
+// Created by nico on 10.06.21.
+//
+
+int main(void)
+{
+        /* TODO */
+
+        return 0;
+}
---
+++ so3/arch/riscv64/include/asm/setup.h
@@ -0,0 +1,10 @@
+//
+// Created by nico on 10.06.21.
+//
+
+#ifndef SO3_SETUP_H
+#define SO3_SETUP_H
+
+void setup_arch(void);
+
+#endif //SO3_SETUP_H
