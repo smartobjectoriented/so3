f0ae26ede1425c5ce673edbc7df5bbd3bffcc8f8
Nicolas Müller <nicolas.muller1@heig-vd.ch>
2021-07-01 15:36:51 +0200

    created csr constant file

---
+++ so3/arch/riscv64/include/asm/csr.h
@@ -0,0 +1,127 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2015 Regents of the University of California
+ */
+
+#ifndef _ASM_RISCV_CSR_H
+#define _ASM_RISCV_CSR_H
+
+/* Status register flags */
+#define SR_SIE			0x00000002 /* Supervisor Interrupt Enable */
+#define SR_MIE			0x00000008 /* Machine Interrupt Enable */
+#define SR_SPIE			0x00000020 /* Previous Supervisor IE */
+#define SR_MPIE			0x00000080 /* Previous Machine IE */
+#define SR_SPP			0x00000100 /* Previously Supervisor */
+#define SR_MPP			0x00001800 /* Previously Machine */
+#define SR_SUM			0x00040000 /* Supervisor User Memory Access */
+
+#define SR_FS			0x00006000 	/* Floating-point Status */
+#define SR_FS_OFF		0x00000000
+#define SR_FS_INITIAL	0x00002000
+#define SR_FS_CLEAN		0x00004000
+#define SR_FS_DIRTY		0x00006000
+
+#define SR_XS			0x00018000	/* Extension Status */
+#define SR_XS_OFF		0x00000000
+#define SR_XS_INITIAL	0x00008000
+#define SR_XS_CLEAN		0x00010000
+#define SR_XS_DIRTY		0x00018000
+
+#define SR_SD		0x8000000000000000 /* FS/XS dirty */
+
+/* SATP flags */
+#define SATP_PPN			0x00000FFFFFFFFFFF
+#define SATP_MODE_39		0x8000000000000000
+#define SATP_MODE			SATP_MODE_39
+#define SATP_ASID_BITS		16
+#define SATP_ASID_SHIFT		44
+#define SATP_ASID_MASK		0xFFFF
+
+/* Exception cause high bit - is an interrupt if set */
+#define CAUSE_IRQ_FLAG		1 << 63
+
+/* Interrupt causes (minus the high bit) */
+#define IRQ_S_SOFT		1
+#define IRQ_M_SOFT		3
+#define IRQ_S_TIMER		5
+#define IRQ_M_TIMER		7
+#define IRQ_S_EXT		9
+#define IRQ_M_EXT		11
+
+/* Exception causes */
+#define EXC_INST_MISALIGNED		0
+#define EXC_INST_ACCESS			1
+#define EXC_BREAKPOINT			3
+#define EXC_LOAD_ACCESS			5
+#define EXC_STORE_ACCESS		7
+#define EXC_SYSCALL				8
+#define EXC_INST_PAGE_FAULT		12
+#define EXC_LOAD_PAGE_FAULT		13
+#define EXC_STORE_PAGE_FAULT	15
+
+/* PMP configuration */
+#define PMP_R			0x01
+#define PMP_W			0x02
+#define PMP_X			0x04
+#define PMP_A			0x18
+#define PMP_A_TOR		0x08
+#define PMP_A_NA4		0x10
+#define PMP_A_NAPOT		0x18
+#define PMP_L			0x80
+
+/* symbolic CSR names: */
+#define CSR_CYCLE		0xc00
+#define CSR_TIME		0xc01
+#define CSR_INSTRET		0xc02
+#define CSR_CYCLEH		0xc80
+#define CSR_TIMEH		0xc81
+#define CSR_INSTRETH	0xc82
+
+#define CSR_SSTATUS		0x100
+#define CSR_SIE			0x104
+#define CSR_STVEC		0x105
+#define CSR_SCOUNTEREN	0x106
+#define CSR_SSCRATCH	0x140
+#define CSR_SEPC		0x141
+#define CSR_SCAUSE		0x142
+#define CSR_STVAL		0x143
+#define CSR_SIP			0x144
+#define CSR_SATP		0x180
+
+#define CSR_MSTATUS		0x300
+#define CSR_MISA		0x301
+#define CSR_MIE			0x304
+#define CSR_MTVEC		0x305
+#define CSR_MSCRATCH	0x340
+#define CSR_MEPC		0x341
+#define CSR_MCAUSE		0x342
+#define CSR_MTVAL		0x343
+#define CSR_MIP			0x344
+#define CSR_PMPCFG0		0x3a0
+#define CSR_PMPADDR0	0x3b0
+#define CSR_MHARTID		0xf14
+
+/* Usable names. At OS level, we want supervisor regs */
+# define CSR_STATUS		CSR_SSTATUS
+# define CSR_IE			CSR_SIE
+# define CSR_TVEC		CSR_STVEC
+# define CSR_SCRATCH	CSR_SSCRATCH
+# define CSR_EPC		CSR_SEPC
+# define CSR_CAUSE		CSR_SCAUSE
+# define CSR_TVAL		CSR_STVAL
+# define CSR_IP			CSR_SIP
+
+# define SR_IE		SR_SIE
+# define SR_PIE		SR_SPIE
+# define SR_PP		SR_SPP
+
+# define RV_IRQ_SOFT	IRQ_S_SOFT
+# define RV_IRQ_TIMER	IRQ_S_TIMER
+# define RV_IRQ_EXT		IRQ_S_EXT
+
+/* IE/IP (Supervisor/Machine Interrupt Enable/Pending) flags */
+#define IE_SIE		(0x1 << RV_IRQ_SOFT)
+#define IE_TIE		(0x1 << RV_IRQ_TIMER)
+#define IE_EIE		(0x1 << RV_IRQ_EXT)
+
+#endif /* _ASM_RISCV_CSR_H */
