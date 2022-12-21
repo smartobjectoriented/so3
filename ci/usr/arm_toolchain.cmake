7df697bc84f7257f96d669a1b69a25ce5eebc062
Daniel Rossier <daniel.rossier@heig-vd.ch>
2022-12-21 10:12:24 +0100

    WiP: CI integration

--- ci/usr/arm_toolchain.cmake
+++ ci/usr/arm_toolchain.cmake
@@ -17,13 +17,13 @@ set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "SO3 usr LDFLAGS for executables")
 set(CMAKE_INSTALL_SO_NO_EXE 0)
 
 # This toolchain file can be used both inside and outside Buildroot.
-set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
-set(CMAKE_C_LINK_EXECUTABLE "arm-linux-gnueabihf-ld <OBJECTS> -o <TARGET>  <LINK_LIBRARIES> <LINK_FLAGS> <LINK_LIBRARIES>")
-set(CMAKE_ASM_COMPILER "arm-linux-gnueabihf-gcc")
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_C_LINK_EXECUTABLE "arm-none-eabi-ld <OBJECTS> -o <TARGET>  <LINK_LIBRARIES> <LINK_FLAGS> <LINK_LIBRARIES>")
+set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
 
 set(CMAKE_C_FLAGS "-Wall -O0 -std=c99  -D_GNU_SOURCE -nostdlib -O0 -pipe -Wall  -D__ARM__ -marm  \
         -mno-thumb-interwork -march=armv5 -g -ffreestanding -fno-common")
  
 set(CMAKE_ASM_FLAGS_DEBUG "-D__ARM__ -D__ASSEMBLY__")
 
-set(CMAKE_LINKER "arm-linux-gnueabihf-ld")
+set(CMAKE_LINKER "arm-none-eabi-ld")
