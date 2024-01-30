1cd4e4a6990f9988544ef0eca4be81e8c410b6fb
Anthony I. Jaccard <anthony.jaccard@heig-vd.ch>
2024-01-30 12:50:03 +0100

    mcount functionnal with timestamps

--- usr/lib/libc/mcount/mcount.c
+++ usr/lib/libc/mcount/mcount.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 /**
  * @brief Timestamps the entry time of the function call identified by its return address
@@ -9,9 +10,11 @@
 */
 void mcount(uintptr_t fn_lr)
 {
-	printf("Entered func with identifier %lx\n", fn_lr);
-	//TODO Get timestamp
-	//TODO Write timestamp with identifier
+	struct timespec ts;
+	// Get timestamp
+	clock_gettime(CLOCK_REALTIME, &ts);
+	// Write timestamp with identifier
+	printf("prof > %lx: %llu,%llu\n", fn_lr, ts.tv_sec, ts.tv_nsec);
 }
 
 /**
@@ -19,8 +22,10 @@ void mcount(uintptr_t fn_lr)
  * @param fn_lr Return address of the profiled function call
 */
 void mcount_exit(uintptr_t fn_lr)
-{
-	printf("Exited func with identifier %lx\n", fn_lr);
-	//TODO Get timestamp
-	//TODO Write timestamp with identifier
+{	
+	struct timespec ts;
+	// Get timestamp
+	clock_gettime(CLOCK_REALTIME, &ts);
+	// Write timestamp with identifier
+	printf("prof < %lx: %llu,%llu\n", fn_lr, ts.tv_sec, ts.tv_nsec);
 }
