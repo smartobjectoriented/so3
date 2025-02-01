94de40fc83d1b31a179c3b03e42bbfaa8afa43f8
André <andre_miguel_costa@hotmail.com>
2025-02-01 18:36:25 +0100

    fix: void function shouldn't return anything

--- usr/src/sh.c
+++ usr/src/sh.c
@@ -97,24 +97,20 @@ void escape_arrow_key(char *buffer, int size) {
  * More secure way and escaped way to get user input
  */
 void get_user_input(char *buffer, int buf_size) {
-    if (buffer == NULL || buf_size <= 0) {
-        return NULL;
-    }
+	if (buffer == NULL || buf_size <= 0) {
+		return;
+	}
 
-	memset(buffer,0,buf_size);
-	
-    if (fgets(buffer, buf_size, stdin) != NULL) {
-		escape_arrow_key(buffer,buf_size);
+	memset(buffer, 0, buf_size);
+
+	if (fgets(buffer, buf_size, stdin) != NULL) {
+		escape_arrow_key(buffer, buf_size);
 		trim(buffer, buf_size);
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
-		
-        return buffer;
-    }
-
-    return NULL;
+		size_t len = strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n') {
+			buffer[len - 1] = '\0';
+		}
+	}
 }
 
 /*
