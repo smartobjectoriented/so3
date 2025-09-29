b5e7040800a171ec8470f31c9899570a5bd4fc01
Daniel Rossier <daniel.rossier@eig-vd.ch>
2025-09-29 16:27:14 +0200

    Remove other functions in exit() which also lead to crash

--- usr/lib/libc/exit/exit.c
+++ usr/lib/libc/exit/exit.c
@@ -27,15 +27,14 @@ weak_alias(libc_exit_fini, __libc_exit_fini);
 
 _Noreturn void exit(int code)
 {
-	__funcs_on_exit();
-	__libc_exit_fini();
-
-#warning __stdio_exit() issue
+	#warning exit() issue
 	/* 
-	 * Currently, this function causes a data abort fault randomly. The effect
+	 * Currently, the following code leads to data abort fault randomly. The effect
 	 * is visible on RPi4 (64-bit) more rarely on virt64
 	 */
 #if 0
+	__funcs_on_exit();
+	__libc_exit_fini();	
 	__stdio_exit();
 #endif /* 0 */
 
