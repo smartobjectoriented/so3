7c0614a0d97783baff2d3fb98bfc67f6e608d20f
Daniel Rossier <daniel.rossier@heig-vd.ch>
2026-07-07 18:30:34 +0200

    refactor(math): consolidate log_2_n helpers into include/math.h
    log_2_n_round_up() and log_2_n_round_down() were defined identically in both
    arch/arm32/include/asm/utils.h and arch/arm64/include/asm/utils.h, yet nothing
    in the tree included either header or called the functions.
    
    Move the two integer-log2 helpers into the generic include/math.h, next to
    round_up_pow2(), and delete the two duplicated and unused arch headers. The
    functions keep their original U-Boot / Texas Instruments attribution.

--- so3/so3/include/math.h
+++ so3/so3/include/math.h
@@ -36,4 +36,38 @@ static inline size_t round_up_pow2(size_t n)
 	return p;
 }
 
+/*
+ * Integer base-2 logarithm of @n, rounded up to the next integer. Originally
+ * from U-Boot -- Copyright (C) 2010 Texas Instruments, Aneesh V <aneesh@ti.com>.
+ */
+static inline s32 log_2_n_round_up(u32 n)
+{
+	s32 log2n = -1;
+	u32 temp = n;
+
+	while (temp) {
+		log2n++;
+		temp >>= 1;
+	}
+
+	if (n & (n - 1))
+		return log2n + 1; /* not power of 2 - round up */
+	else
+		return log2n; /* power of 2 */
+}
+
+/* Integer base-2 logarithm of @n, rounded down (i.e. the index of the MSB). */
+static inline s32 log_2_n_round_down(u32 n)
+{
+	s32 log2n = -1;
+	u32 temp = n;
+
+	while (temp) {
+		log2n++;
+		temp >>= 1;
+	}
+
+	return log2n;
+}
+
 #endif /* MATH_H */
